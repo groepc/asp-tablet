2eb05094d675024d4e5b300b20fdf758b6dfef72
vjanssens <info@vadiemjanssens.nl>
2015-05-22 11:13:52 +0200

    Initial commit

---
+++ Plathe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Plathe.Startup))]
+namespace Plathe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
