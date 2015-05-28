6811a0a4c98b63f3134d4da71150cb66d5beb0dd
Perry Faro <perryfaro@live.nl>
2015-05-28 19:32:39 +0200

    Opzet 4

---
+++ Plathe.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Plathe.WebUI.MvcApplication" Language="C#" %>
--- Plathe.WebUI/Properties/AssemblyInfo.cs
+++ Plathe.WebUI/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("625e6cac-6030-4d4a-b27e-a13aeec030a2")]
+[assembly: Guid("bf3d1314-8abb-4d03-a784-569bca2c7499")]
 
 // Version information for an assembly consists of the following four values:
 //
