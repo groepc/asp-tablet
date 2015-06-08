a04eb2c725dcec4d4d778a5772ce50a0d5d680bc
Perry Faro <perryfaro@live.nl>
2015-06-08 20:45:54 +0200

    CalculatePrice

---
+++ Plathe.Domain/Extensions/CalculatePrice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plathe.Domain.Extensions
+{
+    class CalculatePrice
+    {
+
+        public CalculatePrice(double price, string type)
+        {
+
+            switch (type) {
+
+            }
+        }
+
+    }
+}
